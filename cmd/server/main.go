2fb891e4094e4552a0355afe86b250f6b29e7fae
w3rr0 <konradmateja65@gmail.com>
2025-12-02 15:19:52 +0100

    add change availability endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,6 +16,7 @@ func main() {
 	mux.HandleFunc("/users", handlers.HandleGetUsers)
 	mux.HandleFunc("/create_event", handlers.HandleCreateEvent)
 	mux.HandleFunc("/delete_event", handlers.HandleDeleteEvent)
+	mux.HandleFunc("/change_availability", handlers.HandleChangeAvailability)
 
 	log.Println("Server is running at :8080")
 	err := http.ListenAndServe(":8080", mux)
