35b89bbea3d0d98163363bc66d53e9559aca8fd6
w3rr0 <konradmateja65@gmail.com>
2025-12-08 19:19:17 +0100

    add capitalize func for names

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -136,6 +136,13 @@ func extractNameFromEmail(email string) (string, string, error) {
 	return first, last, nil
 }
 
+func capitalize(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	return strings.ToUpper(string(s[0])) + strings.ToLower(s[1:])
+}
+
 func sendVerificationEmail(to string, token string) error {
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
