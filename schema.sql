ac08f0d036ce58a77c16e1171403aa8788306ccf
w3rr0 <konradmateja65@gmail.com>
2025-12-15 17:48:04 +0100

    fix group column in sql schema

--- schema.sql
+++ schema.sql
@@ -7,11 +7,11 @@ CREATE TYPE group_type AS ENUM
 -- Create Table
 CREATE TABLE users
     ( id SERIAL PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL,
-      email TEXT NOT NULL, “group” group_type NOT NULL );
+      email TEXT NOT NULL, "group" group_type NOT NULL );
 
 -- Example Insert
 INSERT INTO users
-    (first_name, last_name, email, “group”)
+    (first_name, last_name, email, "group")
 VALUES
     ('Jan', 'Kowalski', 'jan.kowalski@iaeste.pl', 'IT'),
     ('Alicja', 'Nowak', 'alicja.nowak@iaeste.pl', 'Grafika');
